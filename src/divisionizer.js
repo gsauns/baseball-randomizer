9ff78dc941e30a5ce46fa37f67415e3d9a24c928
Greg Saunsen <gsauns@gmail.com>
2018-03-22 00:19:58 -0500

    spits out divisions

--- src/divisionizer.js
+++ src/divisionizer.js
@@ -10,18 +10,18 @@ const divisionTwoIndexes = [2, 3, 6, 7, 10, 11];
 export const PLAYERS_IN_LEAGUE = 12;
 
 const leagueMembers = [
-    {key: 'Baseball Stars', weight: 8, data: { weight: 8 }},
-    {key: 'Cincinnati Bowties', weight: 4, data: { weight: 4 }},
-    {key: 'Dateline Predators', weight: 12, data: { weight: 12 }},
-    {key: 'McCann You Dig It?', weight: 6, data: { weight: 6 }},
-    {key: 'Miami Gators', weight: 3, data: { weight: 3 }},
-    {key: 'New York Meats', weight: 11, data: { weight: 11 }},
-    {key: 'Reinstate Rose', weight: 1, data: { weight: 1 }},
-    {key: 'Sons of Pitches', weight: 7, data: { weight: 7 }},
-    {key: 'Speling Bee Champs', weight: 9, data: { weight: 9 }},
-    {key: 'SuckItRedBirds', weight: 2, data: { weight: 2 }},
-    {key: 'Tread Lightly', weight: 10, data: { weight: 10 }},
-    {key: 'The Ulfberht', weight: 5, data: { weight: 5 }}
+    {key: 'Baseball Stars', weight: 8, data: { owner: 'Jason Hughes', weight: 8 }},
+    {key: 'Cincinnati Bowties', weight: 4, data: { owner: 'Jeff Helvie', weight: 4 }},
+    {key: 'Dateline Predators', weight: 12, data: { owner: 'Matt Howes', weight: 12 }},
+    {key: 'McCann You Dig It?', weight: 6, data: { owner: 'Jack Elsey', weight: 6 }},
+    {key: 'Miami Gators', weight: 3, data: { owner: 'Chris Eckel', weight: 3 }},
+    {key: 'New York Meats', weight: 11, data: { owner: 'Greg Saunsen', weight: 11 }},
+    {key: 'Reinstate Rose', weight: 1, data: { owner: 'Chris Gibson', weight: 1 }},
+    {key: 'Sons of Pitches', weight: 7, data: { owner: 'Jason Johnson', weight: 7 }},
+    {key: 'Speling Bee Champs', weight: 9, data: { owner: 'Andy Hubbs', weight: 9 }},
+    {key: 'SuckItRedBirds', weight: 2, data: { owner: 'Judd Hale', weight: 2 }},
+    {key: 'Tread Lightly', weight: 10, data: { owner: 'Mike Brenner', weight: 10 }},
+    {key: 'The Ulfberht', weight: 5, data: { owner: 'Sean Griffin', weight: 5 }}
 ];
 
 export const players = new WeightedList(leagueMembers);
--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ console.log('*** DIVISION GENERATOR ***');
 const outputDivision = (num, division) => {
     console.log('');
     console.log('--- DIVISION ' + num + ' ---');
-    console.log('-----------------');
-    division.forEach(team => console.log(team.key + ' (team weight: ' + team.data.weight + ')'));
+    console.log('------------------');
+    division.forEach(team => console.log(team.key + ', ' + team.data.owner + ' (team weight: ' + team.data.weight + ')'));
+    console.log('------------------');
 }
 
 const divisions = divisionizer.generateDivisions();
 
 outputDivision(1, divisions.divisionOne);
 outputDivision(2, divisions.divisionTwo);
+console.log('');
