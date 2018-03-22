125db8295407d1619ce6843e852c0e3ea49e1b39
Greg Saunsen <gsauns@gmail.com>
2018-03-22 00:05:57 -0500

    working, output to console

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
 import * as divisionizer from './divisionizer';
 
-divisionizer.generateDivisions();
+console.log('*** DIVISION GENERATOR ***');
+
+const outputDivision = (num, division) => {
+    console.log('');
+    console.log('--- DIVISION ' + num + ' ---');
+    console.log('-----------------');
+    division.forEach(team => console.log(team.key + ' (team weight: ' + team.data.weight + ')'));
+}
+
+const divisions = divisionizer.generateDivisions();
+
+outputDivision(1, divisions.divisionOne);
+outputDivision(2, divisions.divisionTwo);
--- test/main.test.js
+++ test/main.test.js
@@ -1,10 +1,15 @@
 import * as divisionizer from '../src/divisionizer';
 
+const divisions = divisionizer.generateDivisions();
+
 test('right amount of players in weighted list', () => {
     expect(divisionizer.players.length).toBe(divisionizer.PLAYERS_IN_LEAGUE);
 });
 
-test('we do a shuffle', () => {
-    divisionizer.generateDivisions();
-    expect(true).toBe(true);
+test('shuffled divisions are evenly divided', () => {
+    expect(divisions.divisionOne.length).toBe(divisions.divisionTwo.length);
+});
+
+test('divisions have expected number of members', () => {
+    expect(divisions.divisionOne.length).toBe(divisionizer.PLAYERS_IN_LEAGUE / 2);
 });
