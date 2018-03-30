3c45aa19869e1951555d90d4685dd5ef56e973e4
Greg Saunsen <gsauns@gmail.com>
2018-03-29 21:55:25 -0500

    moved teams to JSON

--- src/divisionizer.js
+++ src/divisionizer.js
@@ -1,5 +1,6 @@
 'use strict';
 import WeightedList from 'js-weighted-list';
+const fs = require('fs');
 
 // Divisions will be:
 //  1,  4,  5,  8,  9, 12
@@ -9,20 +10,8 @@ const divisionTwoIndexes = [2, 3, 6, 7, 10, 11];
 
 export const PLAYERS_IN_LEAGUE = 12;
 
-const leagueMembers = [
-    {key: 'Baseball Stars', weight: 8, data: { owner: 'Jason Hughes', weight: 8 }},
-    {key: 'Cincinnati Bowties', weight: 4, data: { owner: 'Jeff Helvie', weight: 4 }},
-    {key: 'Dateline Predators', weight: 12, data: { owner: 'Matt Howes', weight: 12 }},
-    {key: 'McCann You Dig It?', weight: 6, data: { owner: 'Jack Elsey', weight: 6 }},
-    {key: 'Miami Gators', weight: 3, data: { owner: 'Chris Eckel', weight: 3 }},
-    {key: 'New York Meats', weight: 11, data: { owner: 'Greg Saunsen', weight: 11 }},
-    {key: 'Reinstate Rose', weight: 1, data: { owner: 'Chris Gibson', weight: 1 }},
-    {key: 'Sons of Pitches', weight: 7, data: { owner: 'Jason Johnson', weight: 7 }},
-    {key: 'Speling Bee Champs', weight: 9, data: { owner: 'Andy Hubbs', weight: 9 }},
-    {key: 'SuckItRedBirds', weight: 2, data: { owner: 'Judd Hale', weight: 2 }},
-    {key: 'Tread Lightly', weight: 10, data: { owner: 'Mike Brenner', weight: 10 }},
-    {key: 'The Ulfberht', weight: 5, data: { owner: 'Sean Griffin', weight: 5 }}
-];
+const league_json = fs.readFileSync('data/league.json');
+const leagueMembers = JSON.parse(league_json);
 
 export const players = new WeightedList(leagueMembers);
 
--- test/main.test.js
+++ test/main.test.js
@@ -1,6 +1,12 @@
 import * as divisionizer from '../src/divisionizer';
+const fs = require('fs');
 
 const divisions = divisionizer.generateDivisions();
+
+test('JSON is read', () => {
+    const league_json = fs.readFileSync('data/league.json');
+    expect(league_json).not.toBeNull();
+})
 
 test('right amount of players in weighted list', () => {
     expect(divisionizer.players.length).toBe(divisionizer.PLAYERS_IN_LEAGUE);
